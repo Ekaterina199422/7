289e946221068ba2e66de8f41cbc97456b6b71da
katerina kulchitskay <rfvb.foto@mail.ru>
2020-11-03 10:16:01 +0200

    new

--- src/main/kotlin/service/WallService.kt
+++ src/main/kotlin/service/WallService.kt
@@ -6,6 +6,7 @@ import ru.netology.wall.*
 class WallService {
     private var postsArray = emptyArray<Post>()
     private var comments = emptyArray<Comment>()
+    private var reports = emptyArray<Report>()
     private var idPost = 0
     private var idComment = 0
 
@@ -20,13 +21,37 @@ class WallService {
         for ((index, posts) in postsArray.withIndex()) {
             if (post.id == posts.id) {
                 postsArray[index] = post.copy(
-                    ownerId = posts.ownerId,
-                    date = posts.date
+                        ownerId = posts.ownerId,
+                        date = posts.date
                 )
                 return true
             }
         }
         return false
+    }
+
+    fun reportComment(commentId: Int, reason: Int) {
+        val reasonList: List<String> = listOf(
+                "спам",
+                "детская порнография",
+                "экстремизм",
+                "насилие",
+                "пропаганда наркотиков",
+                "материал для взрослых",
+                "оскорбление",
+                "призывы к суициду"
+        )
+        try {
+            for (comment in comments) {
+                if (comment.id == commentId) {
+                    reports += Report(reason = reasonList[reason], comment)
+                    return
+                }
+            }
+        } catch (e: ArrayIndexOutOfBoundsException) {
+            throw ReasonArrayIndexOut("index=$reason not found")
+        }
+        throw CommentNotFoundException("comment id=$commentId not found")
     }
 
     fun createComment(postId: Int, comment: Comment) {
