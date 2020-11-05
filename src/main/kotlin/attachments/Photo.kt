143e58174db6dbb36a478609543df17edae711e0
katerina kulchitskay <rfvb.foto@mail.ru>
2020-11-05 10:19:17 +0200

    new

--- src/main/kotlin/attachments/Photo.kt
+++ src/main/kotlin/attachments/Photo.kt
@@ -1,13 +1,20 @@
-package ru.netology.attachments
+package attachments
 
-class Photo(
-    var id: Int,
-    val albumId: Int,
-    val ownerId: Int,
-    val userId: Int,
-    val text: String,
-    val date: Int,
-    val width: Int,
-    val height: Int
-) {
-}
+data class Photo(
+        val id: Int,
+        val albumId: Int,
+        val ownerId: Int,
+        val userId: Int,
+        val text: String,
+        val date: Long,
+        val sizes: Array<PhotoCopy>,
+        val width: Int,
+        val height: Int
+)
+
+data class PhotoCopy(
+        val type : String,
+        val url: String,
+        val width: Int,
+        val height: Int
+)
